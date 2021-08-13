13358cb8f98405adf8cb3feb2836aa09f98bc707
Jess <jessica_yara@yahoo.com.br>
2021-08-13 14:36:42 -0300

    continuação

--- nossa-casa-codigo/src/main/kotlin/br/com/zup/client/Endereco.kt
+++ nossa-casa-codigo/src/main/kotlin/br/com/zup/client/Endereco.kt
@@ -3,14 +3,17 @@ package br.com.zup.client
 import io.micronaut.data.annotation.Embeddable
 
 @Embeddable
-class Endereco(
+class Endereco(enderecoResponse: EnderecoResponse,
     val numero: String,
-    val cep: String,
-    val bairro: String,
-    val cidade: String,
-    val estado:String
+    val cep: String
+){
+    val bairro = enderecoResponse.bairro
+    val cidade = enderecoResponse.cidade
+    val estado = enderecoResponse.estado
+}
 
-)
+
+
 
 
 
--- nossa-casa-codigo/src/main/kotlin/br/com/zup/client/EnderecoResponse.kt
+++ nossa-casa-codigo/src/main/kotlin/br/com/zup/client/EnderecoResponse.kt
@@ -2,22 +2,15 @@ package br.com.zup.client
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-class EnderecoResponse (
+data class EnderecoResponse (
 
-    @JsonProperty("cep")
-    val cep: String,
-    @JsonProperty("bairro")
+
     val bairro: String,
     @JsonProperty("localidade")
     val cidade: String,
     @JsonProperty("uf")
     val estado: String,
     )
-    {
-        fun toEndereco(numero: String): Endereco {
-            return Endereco(numero, cep, bairro, cidade, estado)
-        }
 
-    }
 
 
